654b2daa8f85636ea1209436e7185660e816f535
Jonatas Santos <jonatas.santos@produs.dom>
2021-02-09 02:31:55 -0300

    Refatorado o layout da tela Eventos exibindo os eventos em cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,31 @@
 @extends('layouts.main')
 
-@section('title', 'Worship Events')
+@section('title', 'Gospel Events')
 
 @section('content')
-    <h1>Search for a worship event</h1>
-    <img id="banner" src="/img/banner.jpg" alt="banner">
+<div id="search-container" class="col-md-12">
+    <h1>Busque por um evento gospel</h1>
+    <form action="" method="get">
+        <input type="text" id="seach" class="form-control" placeholder="Procurar...">
+    </form>
+</div> 
 
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p>Veja os eventos dos próximos dias</p>
+            
+    <div id="cards-container" class="row">
+        @foreach ($events as $event)
+            <div class="card col-md-3">
+                <img src="/img/banner.jpg" alt="{{$event->title}}">
+                <div class="card-body">
+                    <div class="card-date">09/02/2021</div>
+                    <div class="card-title">{{$event->title}}</div>
+                    <div class="card-participants">X Participantes</div>
+                    <a href="" class="btn btn-danger btn-more">Saber mais</a>
+                </div>
+            </div>        
+        @endforeach   
+    </div>
+</div> 
 @endsection
